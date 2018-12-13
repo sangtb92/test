0678fa0bbab202df44378370396bdba2b61dd8a6
sangnd <sangnd.it@gmail.com>
2018-12-13 16:47:48 +0700

    fasdf

--- todo/urls.py
+++ todo/urls.py
@@ -4,6 +4,7 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from todo import views
 
 urlpatterns = [
+    path('todos/', views.TodoList.as_view()),
     path('api/todos/', views.TodoList.as_view()),
     path('api/todos/<int:id>', views.TodoList.as_view()),
 ]
